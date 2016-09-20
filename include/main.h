84a71a00666ac9c33bed3cf1a966839132e14b84
Ryan Tse <ryantse@verizon.net>
2016-09-20 11:03:31 -0400

    Add initial files and set up directory structure

---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by blairrobot on 9/20/16.
+//
+
+#ifndef BLOB_TRACK_MAIN_H
+#define BLOB_TRACK_MAIN_H
+
+int main(int, char *[]);
+
+#endif //BLOB_TRACK_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * Finds the blob.
+ *
+ * @author Ryan Tse
+ */
+
+#include "../include/main.h"
+
+using namespace cv;
+
+int main(int argc, char *argv[]) {
+}
