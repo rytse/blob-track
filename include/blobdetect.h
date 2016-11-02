32a9eeb6a908ac441cf057ef0c595abcca52664c
Ryan Tse <rytse@mbhs.edu>
2016-11-02 10:38:38 -0400

    Add script to clean up CMake files

---
+++ include/blobdetect.h
@@ -0,0 +1,13 @@
+//
+// Created by blairrobot on 10/27/16.
+//
+
+using namespace cv;
+
+#ifndef BLOB_TRACK_BLOBDETECT_H
+#define BLOB_TRACK_BLOBDETECT_H
+
+void rectDetect(void);
+SimpleBlobDetector getDetector(void);
+
+#endif //BLOB_TRACK_BLOBDETECT_H
---
+++ src/blobdetect.cpp
@@ -0,0 +1,5 @@
+//
+// Created by blairrobot on 10/27/16.
+//
+
+#include "blobdetect.h"
